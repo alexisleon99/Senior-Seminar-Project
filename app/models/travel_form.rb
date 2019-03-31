ad6a672b08749b58a49dfca671e25d4863583844
alexisleon99 <alexisleon99@gmail.com>
2019-03-31 11:53:21 -0400

    added validations to travel form

--- app/models/travel_form.rb
+++ app/models/travel_form.rb
@@ -1,2 +1,3 @@
 class TravelForm < ApplicationRecord
+	validates :First_Name, :Last_Name, :Department, :Start_Date, :End_Date, :Location, :Flight_Price, :Hotel_Price, :Mileage, :Transportation, :Other, presence: true
 end
