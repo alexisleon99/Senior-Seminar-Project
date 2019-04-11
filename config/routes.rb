b5f8f83a70570352fdaa15a10fbf35423843ff39
jvmiravete <jmiravete1994@hotmail.com>
2019-04-11 20:48:38 +0000

    fixing an issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   
 
-=======
-  devise_for :accounts,  :controllers => { :registrations => 'registrations' }
->>>>>>> 6f3847fe0069d89879022b21bf93774d5af73e5a
   resources :payment_manager_pages
   resources :budget_approver_pages
   resources :request_forms
