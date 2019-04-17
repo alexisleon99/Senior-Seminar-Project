88c9f7064dde516d498db9cfdfe3952b84cb0962
jvmiravete <jmiravete1994@hotmail.com>
2019-04-17 20:41:47 +0000

    updates

--- app/models/travel_form.rb
+++ app/models/travel_form.rb
@@ -1,7 +1,6 @@
 class TravelForm < ApplicationRecord
 	belongs_to :account,:optional => true
 	validates :First_Name, :Last_Name, :Department, :Start_Date, :End_Date, :Location, :Flight_Price, :Hotel_Price, :Mileage, :Transportation, :Other, presence: true
-	has_many :request_forms
 
 	# def total_cost
 	# 	travel_form.cost
