2d997d958d680da69cba7bed237890bd50f2d138
jvmiravete <jmiravete1994@hotmail.com>
2019-04-21 19:16:39 +0000

    pulling agtain

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,5 @@ class Expense < ApplicationRecord
   belongs_to :travel_form, optional: true
   has_many :expenseItems, dependent: :destroy
   has_many :departments
-  has_many :amounts
-  has_many :expenseItems
+  has_many :amounts, dependent: :destroy
 end
