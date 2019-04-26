e14cbe0b38105865dfe643c14769720ee6b77a5c
jvmiravete <jmiravete1994@hotmail.com>
2019-04-26 02:23:00 +0000

    took out mileage; updated where the forms redirected and other small stuff

--- app/helpers/travel_forms_helper.rb
+++ app/helpers/travel_forms_helper.rb
@@ -1,2 +1,3 @@
 module TravelFormsHelper
+
 end
--- app/models/travel_form.rb
+++ app/models/travel_form.rb
@@ -1,6 +1,6 @@
 class TravelForm < ApplicationRecord
 	belongs_to :account,:optional => true
-	validates :First_Name, :Last_Name, :Department, :Start_Date, :End_Date, :Location, :Flight_Price, :Hotel_Price, :Mileage, :Transportation, :Other, presence: true
+	validates :First_Name, :Last_Name, :Department, :Start_Date, :End_Date, :Location, :Flight_Price, :Hotel_Price, :Transportation, :Other, presence: true
 	has_many :request_forms, dependent: :destroy
 	accepts_nested_attributes_for :request_forms, :reject_if => :all_blank, :allow_destroy => true
 end
