6dd79263273794d6e155ff67e94d41e6f2be3e84
jvmiravete <jmiravete1994@hotmail.com>
2019-04-30 21:34:11 +0000

    heroku stuff

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require_relative 'boot'
 
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+#require "active_record/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "sprockets/railtie"
 
-#require "rails/all"
+require "rails/all"
 
 
 # Require the gems listed in Gemfile, including any gems
