95e94e703fb087eb26bc6b6150f6305a093f4791
alexisleon99 <alexisleon99@gmail.com>
2019-05-01 17:16:56 -0400

    fixed other issues

--- app/models/travel_form.rb
+++ app/models/travel_form.rb
@@ -1,9 +1,9 @@
 class TravelForm < ApplicationRecord
 	belongs_to :account,:optional => true
+	validates :Flight_Price, :Hotel_Price, :Transportation, :Other, numericality: true
 	validates :First_Name, :Last_Name, :Department, :Start_Date, :End_Date, :Location, :Flight_Price, :Hotel_Price, :Transportation, :Other, presence: true
 	validates :First_Name, :Last_Name, :Location , format: { with: /\A[a-zA-Z]+\z/,
 	message: "only allows letters" }
-	validates :Flight_Price, :Hotel_Price, :Transportation, :Other, numericality: true
 	validates :End_Date,
 	date: { after: Proc.new { Time.now - 1.day } }
 	has_many :request_forms, dependent: :destroy
