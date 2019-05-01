ca00ca26ccf50f8338e668241777c58e41d6136b
alexisleon99 <alexisleon99@gmail.com>
2019-05-01 16:16:25 -0400

    time validation in place

--- app/models/travel_form.rb
+++ app/models/travel_form.rb
@@ -1,6 +1,8 @@
 class TravelForm < ApplicationRecord
 	belongs_to :account,:optional => true
 	validates :First_Name, :Last_Name, :Department, :Start_Date, :End_Date, :Location, :Flight_Price, :Hotel_Price, :Transportation, :Other, presence: true
+	validates :End_Date,
+	date: { after: Proc.new { Time.now - 1.day } }
 	has_many :request_forms, dependent: :destroy
 	accepts_nested_attributes_for :request_forms, :reject_if => :all_blank, :allow_destroy => true
 end
